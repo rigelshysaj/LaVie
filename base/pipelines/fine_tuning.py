ada0836848f6acd9dda7d476aedc41838689ec53
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-05-30 15:06:08 +0200

    lora

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -147,6 +147,10 @@ def train_lora_model(data, video_folder, args):
         r=16, lora_alpha=32, lora_dropout=0.1, bias="none", task_type="CAUSAL_LM"
     )
     
+
+    unet_config = unet.config.to_dict()
+    unet_config['model_type'] = 'unet'
+
     unet = get_peft_model(unet, lora_config)
     
     #dataset = VideoDatasetMsrvtt(data, video_folder)
