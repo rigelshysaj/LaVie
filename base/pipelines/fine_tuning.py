3c214f455447de8f94aa26f148d868a4dce87239
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-05-31 22:30:09 +0200

    solo due video

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -222,10 +222,10 @@ def train_lora_model(data, video_folder, args):
                     remainder = image_batch_size % text_batch_size
                     if remainder != 0:
                         text_features = torch.cat([text_features, text_features[:remainder]], dim=0)
+            '''
 
             image_features = image_features.unsqueeze(1).repeat(1, text_features.size(1), 1)
             print(f"Reshaped image_features shape: {image_features.shape}")
-            '''
 
             encoder_hidden_states = torch.cat([text_features, image_features], dim=-1)
 
