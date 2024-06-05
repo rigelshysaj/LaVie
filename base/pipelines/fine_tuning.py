d86bfab6c9b99d17c47a76d36da73ad30cbd6d94
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-06-05 19:59:45 +0200

    solo due video

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -201,8 +201,8 @@ def train_lora_model(data, video_folder, args):
             print(f"text_features shape: {text_features.shape}")
 
             image_inputs = clip_processor(images=frame_tensor, return_tensors="pt").pixel_values.to(unet.device)
-            outputs = clip_model.get_image_features(image_inputs)
-            image_features = outputs.image_embeds.to(torch.float16)
+            outputs = clip_model(image_inputs, output_hidden_states=True)
+            image_features = outputs.last_hidden_state[:, 0, :].to(torch.float16)
             hidden_states = outputs.hidden_states
 
             # Ottieni l'ultimo hidden state
