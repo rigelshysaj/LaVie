126b17624f9e2d73eaae5525b354d65c7c2e4d4a
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-06-12 16:23:01 +0200

    solo due video

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -201,8 +201,10 @@ def train_lora_model(data, video_folder, args):
             print(f"text_features shape: {text_features.shape}")
 
             image_inputs = clip_processor(images=frame_tensor, return_tensors="pt").pixel_values.to(unet.device)
-            image_features = clip_model.get_image_features(image_inputs).to(torch.float16)
+            image_outputs = clip_model(image_inputs, output_hidden_states=True)
+            image_features = image_outputs.hidden_states[-1].to(torch.float16)
             print(f"image_features shape: {image_features.shape}")
+
 
             
             '''
