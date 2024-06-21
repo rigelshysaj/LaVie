960f4b519bb4988215102d58c14d68f569d1074e
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-06-21 11:04:27 +0200

    video

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -278,8 +278,8 @@ def train_lora_model(data, video_folder, args):
 
                 print(f"timestep shape: {timestep.shape}, dtype: {timestep.dtype}")
 
-                #sample=torch.randn(2, 4, 16, 40, 64).to(unet.device, dtype=torch.float16)
-                sample=torch.randn(2, 3, 30, 224, 224).to(unet.device, dtype=torch.float16)
+                sample=torch.randn(2, 4, 16, 40, 64).to(unet.device, dtype=torch.float16)
+                #sample=torch.randn(2, 4, 21, 32, 32).to(unet.device, dtype=torch.float16)
 
                 # Forward pass
                 output = unet(
