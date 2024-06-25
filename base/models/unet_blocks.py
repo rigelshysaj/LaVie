d80621f76c8bc16bb2d686420a05658184c9c6f2
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-06-25 21:24:08 +0200

    _encode_prompt

--- base/models/unet_blocks.py
+++ base/models/unet_blocks.py
@@ -327,8 +327,8 @@ class CrossAttnDownBlock3D(nn.Module):
     def forward(self, hidden_states, temb=None, encoder_hidden_states=None, attention_mask=None, use_image_num=None):
         output_states = ()
 
-        #print(f"hidden_states shape: {hidden_states.shape}, dtype: {hidden_states.dtype}") #torch.Size([1, 320, 16, 40, 64]), dtype: torch.float16
-        #print(f"encoder_hidden_states shape: {encoder_hidden_states.shape}, dtype: {encoder_hidden_states.dtype}") #shape: torch.Size([1, 8, 768]), dtype: torch.float16
+        print(f"hidden_states shape: {hidden_states.shape}, dtype: {hidden_states.dtype}") #torch.Size([1, 320, 16, 40, 64]), dtype: torch.float16
+        print(f"encoder_hidden_states shape: {encoder_hidden_states.shape}, dtype: {encoder_hidden_states.dtype}") #shape: torch.Size([1, 8, 768]), dtype: torch.float16
 
         #Però quando il shape di hidden_states è [1, 320, 16, 20, 32] le dimensioni di sotto cambiano, diventano: encoder_hidden_states shape: torch.Size([1, 8, 768]), dtype: torch.float16, hidden_states1 shape: torch.Size([1, 640, 16, 20, 32]), dtype: torch.float16, hidden_states2 shape: torch.Size([1, 640, 16, 20, 32]), dtype: torch.float16, hidden_states1 shape: torch.Size([1, 640, 16, 20, 32]), dtype: torch.float16, hidden_states2 shape: torch.Size([1, 640, 16, 20, 32]), dtype: torch.float16, hidden_states5 shape: torch.Size([1, 640, 16, 10, 16]), dtype: torch.float16, hidden_states shape: torch.Size([1, 640, 16, 10, 16]), dtype: torch.float16, encoder_hidden_states shape: torch.Size([1, 8, 768]), dtype: torch.float16, hidden_states1 shape: torch.Size([1, 1280, 16, 10, 16]), dtype: torch.float16, hidden_states2 shape: torch.Size([1, 1280, 16, 10, 16]), dtype: torch.float16, hidden_states1 shape: torch.Size([1, 1280, 16, 10, 16]), dtype: torch.float16, hidden_states2 shape: torch.Size([1, 1280, 16, 10, 16]), dtype: torch.float16, hidden_states5 shape: torch.Size([1, 1280, 16, 5, 8]), dtype: torch.float16
 
@@ -354,20 +354,20 @@ class CrossAttnDownBlock3D(nn.Module):
                     return custom_forward
 
                 hidden_states = torch.utils.checkpoint.checkpoint(create_custom_forward(resnet), hidden_states, temb)
-                #print(f"hidden_states1 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}") #shape: torch.Size([1, 320, 16, 40, 64]), dtype: torch.float16
+                print(f"hidden_states1 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}") #shape: torch.Size([1, 320, 16, 40, 64]), dtype: torch.float16
 
                 hidden_states = torch.utils.checkpoint.checkpoint(
                     create_custom_forward_attn(attn, return_dict=False, use_image_num=use_image_num),
                     hidden_states,
                     encoder_hidden_states,
                 )[0]
-                #print(f"hidden_states2 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}") #shape: torch.Size([1, 320, 16, 40, 64]), dtype: torch.float16
+                print(f"hidden_states2 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}") #shape: torch.Size([1, 320, 16, 40, 64]), dtype: torch.float16
 
             else:
                 hidden_states = resnet(hidden_states, temb)
-                #print(f"hidden_states3 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}")
+                print(f"hidden_states3 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}")
                 hidden_states = attn(hidden_states, encoder_hidden_states=encoder_hidden_states, use_image_num=use_image_num).sample
-                #print(f"hidden_states4 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}")
+                print(f"hidden_states4 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}")
 
 
             output_states += (hidden_states,)
@@ -375,12 +375,12 @@ class CrossAttnDownBlock3D(nn.Module):
         if self.downsamplers is not None:
             for downsampler in self.downsamplers:
                 hidden_states = downsampler(hidden_states)
-                #print(f"hidden_states5 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}") #shape: torch.Size([1, 320, 16, 20, 32]), dtype: torch.float16
+                print(f"hidden_states5 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}") #shape: torch.Size([1, 320, 16, 20, 32]), dtype: torch.float16
 
 
             output_states += (hidden_states,)
         
-        #print(f"hidden_states6 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}")
+        print(f"hidden_states6 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}")
 
         return hidden_states, output_states
 
@@ -438,7 +438,7 @@ class DownBlock3D(nn.Module):
         self.gradient_checkpointing = False
 
     def forward(self, hidden_states, temb=None):
-        print(f"hidden_states1 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}")
+        #print(f"hidden_states1 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}") #shape: torch.Size([1, 1280, 16, 5, 8]), dtype: torch.float16
         output_states = ()
 
         for resnet in self.resnets:
@@ -451,21 +451,21 @@ class DownBlock3D(nn.Module):
                     return custom_forward
 
                 hidden_states = torch.utils.checkpoint.checkpoint(create_custom_forward(resnet), hidden_states, temb)
-                print(f"hidden_states2 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}")
+                #print(f"hidden_states2 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}") #shape: torch.Size([1, 1280, 16, 5, 8]), dtype: torch.float16
             else:
                 hidden_states = resnet(hidden_states, temb)
-                print(f"hidden_states3 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}")
+                #print(f"hidden_states3 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}")
 
             output_states += (hidden_states,)
 
         if self.downsamplers is not None:
             for downsampler in self.downsamplers:
                 hidden_states = downsampler(hidden_states)
-                print(f"hidden_states4 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}")
+                #print(f"hidden_states4 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}")
 
             output_states += (hidden_states,)
 
-        print(f"hidden_states5 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}")
+        #print(f"hidden_states5 shape: {hidden_states.shape}, dtype: {hidden_states.dtype}") #shape: torch.Size([1, 1280, 16, 5, 8]), dtype: torch.float16
 
         return hidden_states, output_states
 
