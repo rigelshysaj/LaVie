cccabed261827ca97ce5e4046fcb9b2475d14cc1
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-07-13 22:33:16 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -657,7 +657,7 @@ def main(args):
     print(f"image_tensor shape: {image_tensor.shape}, dtype: {image_tensor.dtype}")
 
     video = inference(unet, tokenizer, text_encoder, vae, clip_model, clip_processor, noise_scheduler, description, image_tensor, device, guidance_scale=7.5).video
-    imageio.mimwrite(args.output_folder + '.mp4', video[0], fps=8, quality=9) # highest quality is 10, lowest is 0
+    imageio.mimwrite(args.output_folder + 'ozottt' + '.mp4', video[0], fps=8, quality=9) # highest quality is 10, lowest is 0
 
     print('save path {}'.format(args.output_folder))
     
