dd3cc57165520c9a19c98d5abc31560e37dedd6b
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-07-20 19:17:16 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -520,6 +520,8 @@ def train_lora_model(data, video_folder, args):
 
             assert text_features.dtype == last_hidden_state.dtype, "text_features and last_hidden_state must have the same dtype"
 
+            attention_layer = attention_layer.to(torch.float16)
+
             # Calcola l'attenzione
             attention_output, _ = attention_layer(text_features, last_hidden_state, last_hidden_state)
 
