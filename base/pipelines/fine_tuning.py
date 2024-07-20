9c8f8d488a35f4cd68c79d00f9055f1093560bff
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-07-20 19:24:14 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -520,9 +520,11 @@ def train_lora_model(data, video_folder, args):
 
             assert text_features.dtype == last_hidden_state.dtype, "text_features and last_hidden_state must have the same dtype"
 
-            with torch.cuda.amp.autocast():
+            text_features = text_features.to(torch.float16)
+            last_hidden_state = last_hidden_state.to(torch.float16)
+
             # Calcola l'attenzione
-                attention_output, _ = attention_layer(text_features, last_hidden_state, last_hidden_state)
+            attention_output, _ = attention_layer(text_features, last_hidden_state, last_hidden_state)
 
             #print(f"attention_output shape: {attention_output.shape}, dtype: {attention_output.dtype}") #[10, 1, 768] torch.float16
             
