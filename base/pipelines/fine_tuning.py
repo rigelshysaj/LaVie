09439be783deb68844bc11494c6f3bf6f799c5ba
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-08-04 14:33:45 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -420,9 +420,7 @@ def train_lora_model(data, video_folder, args):
     lora_config = LoraConfig(
         r=32,
         lora_alpha=16,
-        target_modules=["attn2.to_q", "attn2.to_k", "attn2.to_v", "attn2.to_out.0"],
-        lora_dropout=0.1,
-        bias="none"
+        target_modules=["attn2.to_q", "attn2.to_k", "attn2.to_v", "attn2.to_out.0"]
     )
 
     unet = get_peft_model(unet, lora_config)
