850353db722e139a642231a6129289895bfa96f4
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-08-06 15:48:00 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -22,7 +22,7 @@ import torch.nn.functional as F
 import einops
 from diffusers.models import AutoencoderKL
 from torch.utils.checkpoint import checkpoint
-from diffusers.schedulers import DDIMScheduler, DDPMScheduler
+from diffusers.schedulers import DDIMScheduler, DDPMScheduler, PNDMScheduler, EulerDiscreteScheduler
 from transformers import get_cosine_schedule_with_warmup
 from dataclasses import dataclass
 from peft import PeftModel, LoraConfig
@@ -46,29 +46,70 @@ logger = logging.get_logger(__name__)
 class StableDiffusionPipelineOutput(BaseOutput):
     video: torch.Tensor
 
-def load_model_for_inference(device, args):
+def load_model_for_inference(args):
+	if args.seed is not None:
+		torch.manual_seed(args.seed)
+	torch.set_grad_enabled(False)
+	device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    sd_path = args.pretrained_path + "/stable-diffusion-v1-4"
+	sd_path = args.pretrained_path + "/stable-diffusion-v1-4"
+	unet = get_models(args, sd_path).to(device, dtype=torch.float16)
+	state_dict = find_model(args.ckpt_path)
+	unet.load_state_dict(state_dict)
+	
+	vae = AutoencoderKL.from_pretrained(sd_path, subfolder="vae", torch_dtype=torch.float16).to(device)
+	tokenizer_one = CLIPTokenizer.from_pretrained(sd_path, subfolder="tokenizer")
+	text_encoder_one = CLIPTextModel.from_pretrained(sd_path, subfolder="text_encoder", torch_dtype=torch.float16).to(device) # huge
 
-    # Carica il modello UNet e applica LoRA
-    unet = get_models(args, sd_path).to(device, dtype=torch.float16)
- 
-    
-    unet.eval()
-    
-    # Carica gli altri componenti necessari
-    tokenizer = CLIPTokenizer.from_pretrained(sd_path, subfolder="tokenizer")
-    text_encoder = CLIPTextModel.from_pretrained(sd_path, subfolder="text_encoder", torch_dtype=torch.float16).to(device)
-    vae = AutoencoderKL.from_pretrained(sd_path, subfolder="vae", torch_dtype=torch.float16).to(device)
-    clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
-    clip_processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
-    noise_scheduler = DDPMScheduler.from_pretrained(sd_path, subfolder="scheduler")
+	# set eval mode
+	unet.eval()
+	vae.eval()
+	text_encoder_one.eval()
+	
+	if args.sample_method == 'ddim':
+		scheduler = DDIMScheduler.from_pretrained(sd_path, 
+											   subfolder="scheduler",
+											   beta_start=args.beta_start, 
+											   beta_end=args.beta_end, 
+											   beta_schedule=args.beta_schedule)
+	elif args.sample_method == 'eulerdiscrete':
+		scheduler = EulerDiscreteScheduler.from_pretrained(sd_path,
+											   subfolder="scheduler",
+											   beta_start=args.beta_start,
+											   beta_end=args.beta_end,
+											   beta_schedule=args.beta_schedule)
+	elif args.sample_method == 'ddpm':
+		scheduler = DDPMScheduler.from_pretrained(sd_path,
+											  subfolder="scheduler",
+											  beta_start=args.beta_start,
+											  beta_end=args.beta_end,
+											  beta_schedule=args.beta_schedule)
+	else:
+		raise NotImplementedError
 
-    text_encoder.eval()
-    vae.eval()
-    clip_model.eval()
+	videogen_pipeline = VideoGenPipeline(vae=vae, 
+								 text_encoder=text_encoder_one, 
+								 tokenizer=tokenizer_one, 
+								 scheduler=scheduler, 
+								 unet=unet).to(device)
+	videogen_pipeline.enable_xformers_memory_efficient_attention()
+
+	if not os.path.exists(args.output_folder):
+		os.makedirs(args.output_folder)
+
+	video_grids = []
+	for prompt in args.text_prompt:
+		print('Processing the ({}) prompt'.format(prompt))
+		videos = videogen_pipeline(prompt, 
+								video_length=args.video_length, 
+								height=args.image_size[0], 
+								width=args.image_size[1], 
+								num_inference_steps=args.num_sampling_steps,
+								guidance_scale=args.guidance_scale).video
+		imageio.mimwrite(args.output_folder + prompt.replace(' ', '_') + '.mp4', videos[0], fps=8, quality=9) # highest quality is 10, lowest is 0
+	
+	print('save path {}'.format(args.output_folder))
     
-    return unet, tokenizer, text_encoder, vae, clip_model, clip_processor, noise_scheduler
 
 
 class VideoDatasetMsvd(Dataset):
@@ -304,26 +345,10 @@ def train_lora_model(data, video_folder, args):
     return unet
     
 
-def main(args):
-    
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    unet, tokenizer, text_encoder, vae, clip_model, clip_processor, noise_scheduler = load_model_for_inference(device, args)
-
-    videogen_pipeline = VideoGenPipeline(vae=vae, text_encoder=text_encoder, tokenizer=tokenizer, scheduler=noise_scheduler, unet=unet).to(device)
-	
-    videogen_pipeline.enable_xformers_memory_efficient_attention()
-
-    video = videogen_pipeline("a cat meow at the screen", video_length=16, height=320, width=512, num_inference_steps=50, guidance_scale=7.5).video
-
-    imageio.mimwrite(args.output_folder + 'efundit' + '.mp4', video[0], fps=8, quality=9) # highest quality is 10, lowest is 0
-
-    print('save path {}'.format(args.output_folder))
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, default="")
     args = parser.parse_args()
 
-    main(OmegaConf.load(args.config))
+    load_model_for_inference(OmegaConf.load(args.config))
