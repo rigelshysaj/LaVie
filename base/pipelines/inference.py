71baa685ce21d429e4651683636986485c9991b5
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-08-06 23:08:02 +0200

    main

--- base/pipelines/inference.py
+++ base/pipelines/inference.py
@@ -539,6 +539,8 @@ class VideoGenPipeline(DiffusionPipeline):
         
         print("-----------------------aaaaaaaa-------------------------------")
         print(image_path)
+        print("-----")
+        print(prompt)
         print("-----------------------bbbbbbbb-------------------------------")
         
         # 0. Default height and width to unet
