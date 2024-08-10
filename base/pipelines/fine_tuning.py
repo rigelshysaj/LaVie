ff8ff6b6ac5c6eff4ec0984cfd280729997ad6f1
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-08-10 15:11:30 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -31,9 +31,8 @@ import torch.nn as nn
 import numpy as np
 import torchvision.models as models
 import torch.nn.functional as F
-from diffusers.utils import check_min_version, convert_state_dict_to_diffusers, is_wandb_available
 import einops
-from diffusers import AutoencoderKL, DiffusionPipeline, StableDiffusionPipeline
+from diffusers import AutoencoderKL, DiffusionPipeline, StableDiffusionPipeline, convert_state_dict_to_diffusers
 from torch.utils.checkpoint import checkpoint
 from diffusers.schedulers import DDIMScheduler, DDPMScheduler, PNDMScheduler, EulerDiscreteScheduler
 from transformers import get_cosine_schedule_with_warmup
