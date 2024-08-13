acc9c8b4e9df9c9f4a2b3c9d91b09a7f14245b10
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-08-13 22:58:47 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -521,7 +521,7 @@ def train_lora_model(data, video_folder, args):
 
                 print(f"description: {list(description)}")
                 # Get the text embedding for conditioning
-                text_inputs = tokenizer(list(description), return_tensors="pt", padding=True, truncation=True).input_ids.to(unet.device)
+                text_inputs = tokenizer(description[0], return_tensors="pt", padding=True, truncation=True).input_ids.to(unet.device)
                 text_features = text_encoder(text_inputs, return_dict=False)[0]
                 print(f"train_lora_model text_features shape: {text_features.shape}, dtype: {text_features.dtype}") #[1, 10, 768] torch.float16
 
