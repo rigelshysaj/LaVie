d5c1d47352762ea8543c7a69448b3abb628d7eab
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-08-14 16:27:47 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -606,7 +606,7 @@ def train_lora_model(data, video_folder, args):
                 accelerator.log({"train_loss": train_loss}, step=global_step)
                 train_loss = 0.0
 
-                if (epoch + 1) % 100 == 0:
+                if (epoch + 1) % 50 == 0:
                     if accelerator.is_main_process:
                         # _before_ saving state, check if this save would set us over the `checkpoints_total_limit`
                         if args.checkpoints_total_limit is not None:
@@ -671,7 +671,7 @@ def train_lora_model(data, video_folder, args):
         print(f"Epoch {epoch}/{args.num_train_epochs} completed with average loss: {avg_epoch_loss}")
         epoch_losses.append(avg_epoch_loss)      
 
-        if (epoch + 1) % 100 == 0:
+        if (epoch + 1) % 50 == 0:
             with torch.no_grad():
 
                 videogen_pipeline = VideoGenPipeline(vae=vae, 
