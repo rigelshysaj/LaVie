17f45f0b27f39029f11f17469c23b3409338de6b
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-08-16 19:56:10 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -678,7 +678,7 @@ def train_lora_model(data, video_folder, args):
         print(f"Epoch {epoch}/{args.num_train_epochs} completed with average loss: {avg_epoch_loss}")
         epoch_losses.append(avg_epoch_loss)      
 
-        if (epoch + 1) % 300 == 0:
+        if (epoch + 1) % 2 == 0:
             with torch.no_grad():
 
                 videogen_pipeline = VideoGenPipeline(vae=vae, 
