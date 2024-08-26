c9b5abe43dd10ed35cacd6ab32f97b90f5ae398f
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-08-26 15:05:19 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -129,7 +129,7 @@ def load_model_for_inference(args):
     accelerator.print(f"Inference from checkpoint {path}")
     accelerator.load_state(os.path.join(args.output_dir, path))
 
-    image_path = "/content/drive/My Drive/shoot.jpeg"
+    image_path = "/content/drive/My Drive/rabbit.webp"
 
     image = Image.open(image_path)
 
