25d0003cffa7138e03ec9331eab1493993eeaddd
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-08-26 15:22:22 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -129,9 +129,7 @@ def load_model_for_inference(args):
     accelerator.print(f"Inference from checkpoint {path}")
     accelerator.load_state(os.path.join(args.output_dir, path))
 
-    image_path = "/content/drive/My Drive/rabbit2.jpeg"
-
-    image = Image.open(image_path)
+    image = Image.open(args.image_path)
 
     # Definisci la trasformazione
     transform = transforms.Compose([
