f0b6e35087bc04120dc256a9899c91560d0f57fc
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-09-03 22:09:01 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -695,7 +695,7 @@ def lora_model(data, video_folder, args, training=True):
                     print(f"attention_weights shape: {attention_weights.shape}, dtype: {attention_weights.dtype}")
                     print(f"frame_tensor shape: {frame_tensor.shape}, dtype: {frame_tensor.dtype}")
 
-                    visualize_attention(frame_tensor, attention_weights, '/content/drive/My Drive/attention_visualization.png')
+                    visualize_attention(frame_tensor, attention_weights, f'/content/drive/My Drive/attention_visualization_{step}_{global_step}.png')
                     encoder_hidden_states = encoder_hidden_states.transpose(0, 1)
 
 
