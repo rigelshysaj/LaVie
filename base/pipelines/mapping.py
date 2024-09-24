ff237454b5f491d8ecfdef47f1da91195cff6640
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-09-24 22:24:44 +0200

    main

--- base/pipelines/mapping.py
+++ base/pipelines/mapping.py
@@ -81,33 +81,6 @@ class MappingNetwork(nn.Module):
         return x
     
     
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-
-
-class MappingNetwork(nn.Module):
-    def __init__(self, input_dim=1024, output_dim=768, hidden_dims=[1024, 512, 512, 256]):
-        super(MappingNetwork, self).__init__()
-        layers = []
-        current_dim = input_dim
-        for hidden_dim in hidden_dims:
-            layers.append(nn.Linear(current_dim, hidden_dim))
-            layers.append(nn.LeakyReLU(0.2))
-            layers.append(nn.BatchNorm1d(hidden_dim))
-            layers.append(nn.Dropout(0.2))
-            current_dim = hidden_dim
-        layers.append(nn.Linear(current_dim, output_dim))
-        self.mapping = nn.Sequential(*layers)
-        
-    def forward(self, x):
-        # x: [batch_size, num_patches, 1024]
-        batch_size, num_patches, _ = x.size()
-        x = x.view(batch_size * num_patches, -1)
-        x = self.mapping(x)
-        x = x.view(batch_size, num_patches, -1)
-        return x
 
 
 def training_mapping(train_dataloader, val_dataloader, clip_model, clip_processor, tokenizer, text_encoder, device):
