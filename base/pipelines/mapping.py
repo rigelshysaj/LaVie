5aee45b48d07b9cfb3c270b1f15f38eb2566c022
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-09-24 14:28:28 +0200

    main

--- base/pipelines/mapping.py
+++ base/pipelines/mapping.py
@@ -129,10 +129,10 @@ def training_mapping(train_dataloader, val_dataloader, clip_model, clip_processo
                     pixel_values=image_inputs,
                 ).last_hidden_state
 
-            print(f"image_embeddings shape: {image_embeddings.shape}, dtype: {image_embeddings.dtype}")
+            #print(f"image_embeddings shape: {image_embeddings.shape}, dtype: {image_embeddings.dtype}")
             # Mappa le embedding delle immagini
             mapped_image_embeddings = mapping_network(image_embeddings)  # [batch_size, 257, 768]
-            print(f"mapped_image_embeddings shape: {mapped_image_embeddings.shape}, dtype: {mapped_image_embeddings.dtype}")
+            #print(f"mapped_image_embeddings shape: {mapped_image_embeddings.shape}, dtype: {mapped_image_embeddings.dtype}")
             
             cosine_sim = F.cosine_similarity(mapped_image_embeddings, text_embeddings, dim=-1)  # [batch_size, seq_len]
             loss = (1 - cosine_sim).mean()
