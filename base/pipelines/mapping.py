e68e64e428165630c12ad574e3f19d1a4688085a
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-09-25 00:27:19 +0200

    main

--- base/pipelines/mapping.py
+++ base/pipelines/mapping.py
@@ -125,11 +125,11 @@ def training_mapping(train_dataloader, val_dataloader, clip_model, clip_processo
                 ).last_hidden_state
 
 
-            print(f"image_embeddings shape: {image_embeddings.shape}, dtype: {image_embeddings.dtype}")
+            #print(f"image_embeddings shape: {image_embeddings.shape}, dtype: {image_embeddings.dtype}")
             # Mappa le embedding delle immagini
             mapped_image_embeddings = mapping_network(image_embeddings)  # [batch_size, 257, 768]
 
-            print(f"mapped_image_embeddings shape: {mapped_image_embeddings.shape}, dtype: {mapped_image_embeddings.dtype}")
+            #print(f"mapped_image_embeddings shape: {mapped_image_embeddings.shape}, dtype: {mapped_image_embeddings.dtype}")
 
             # Usa le embedding del token [CLS]
             mapped_image_embeddings_pooled = mapped_image_embeddings.mean(dim=1)
