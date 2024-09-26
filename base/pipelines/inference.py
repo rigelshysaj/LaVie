182d038d1d2d6e6e492ae1186380e412f04efe3f
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-09-26 18:38:19 +0200

    main

--- base/pipelines/inference.py
+++ base/pipelines/inference.py
@@ -297,8 +297,11 @@ class VideoGenPipeline(DiffusionPipeline):
             similarity = compute_cosine_similarity(prompt_embeds, mapped_image_features)
             print(f"Inference Cosine Similarity between text and image embeddings: {similarity}")
 
+            alpha = 0.5  # puoi regolare questo valore
+            interpolated_features = alpha * prompt_embeds + (1 - alpha) * mapped_image_features
+
             # Applica il cross-attention
-            prompt_embeds = mapped_image_features
+            prompt_embeds = interpolated_features
 
             
 
