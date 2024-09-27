95de2141b16a78f59419904157fcd830db2618b1
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-09-27 15:14:53 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -629,8 +629,10 @@ def lora_model(data, video_folder, args, training=True):
 
                     alpha = 0.5  # puoi regolare questo valore
                     interpolated_features = alpha * text_features + (1 - alpha) * mapped_image_features
+
+                    combined_features = torch.cat([text_features, mapped_image_features], dim=1)
                     
-                    encoder_hidden_states = interpolated_features
+                    encoder_hidden_states = combined_features
                     
                     
                     #print(f"encoder_hidden_states shape: {encoder_hidden_states.shape}, dtype: {encoder_hidden_states.dtype}") 
