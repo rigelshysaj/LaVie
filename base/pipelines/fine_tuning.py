3bb0162c548f762e8870988ba5d28a9ba0419d8e
Rigels Hysaj <hysaj.rigels@gmail.com>
2024-10-12 23:24:20 +0200

    main

--- base/pipelines/fine_tuning.py
+++ base/pipelines/fine_tuning.py
@@ -927,4 +927,4 @@ if __name__ == "__main__":
     video_folder = os.path.join(dataset_path, 'YouTubeClips')
     data = os.path.join(dataset_path, 'annotations.txt')
     
-    lora_model(data, video_folder, OmegaConf.load(args.config), method=1)
+    lora_model(data, video_folder, OmegaConf.load(args.config), method=2)
